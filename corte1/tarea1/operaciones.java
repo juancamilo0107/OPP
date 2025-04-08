6842f175d23ff3bb4ee2e4e351ea8927e26292a7
juancamilo0107 <jcalvarezr@udistrital.edu.co>
2025-04-08 17:45:51 -0500

    Add files via upload

---
+++ corte1/tarea1/operaciones.java
@@ -0,0 +1,21 @@
+package calculadora.magica;
+
+public class operaciones {
+ 
+    int a, b, r;
+   
+    public int sumar(int a, int b) {
+        r = a + b;
+        return r;  
+    }
+
+    public int restar(int a, int b) {
+        r = a - b;
+        return r;  
+    }
+
+    public int multiplicar(int a, int b) {
+        r = a * b;
+        return r;  
+    }
+}
