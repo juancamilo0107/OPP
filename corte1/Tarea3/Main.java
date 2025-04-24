73d8c1296fbba6b0a76d47412542f32602bdf8d5
juancamilo0107 <jcalvarezr@udistrital.edu.co>
2025-04-24 17:45:05 -0500

    Add files via upload

---
+++ corte1/Tarea3/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// Clase principal del programa
+public class Main {
+    public static void main(String[] args) {
+        // Usamos Scanner para leer los números que el usuario escriba
+            Scanner scanner = new Scanner(System.in);
+            // Pedimos el primer número
+            System.out.print("Ingrese el primer número: ");
+            int numero1 = scanner.nextInt();
+
+            // Pedimos el segundo número
+            System.out.print("Ingrese el segundo número: ");
+            int numero2 = scanner.nextInt();
+
+            // Creamos un objeto de la clase Operations y le pasamos los dos números
+            Operaciones operacion = new Operaciones(numero1, numero2);
+
+            // Mostramos los resultados de las operaciones matemáticas básicas
+            operacion.sumar();
+            System.out.println(operacion.getResult());
+            /*
+            System.out.println("Suma: " + operacion.sumar());
+            System.out.println("Resta: " + operacion.restar());
+            System.out.println("Multiplicación: " + operacion.multiplicar());
+            */
+        }        
+}
