949ffb66912c3d385dfc3aac80606e51a1f1db2f
juancamilo0107 <jcalvarezr@udistrital.edu.co>
2025-04-29 16:53:44 -0500

    Add files via upload

---
+++ corte1/Tarea3/Values.java
@@ -0,0 +1,26 @@
+public class Values {
+    private double a; 
+    private double b;
+    private double result;
+    
+    public double getA(){
+        return a;
+    }
+    public void setA(double a){
+    this.a=a;
+    }
+    public double getB(){
+    
+        return b;
+    }
+    public void setB(double b){
+        this.b=b;
+    }
+    public double getR(){
+    System.out.print(result);
+    return result;
+    }
+    public void setR(double result){
+    this.result=result;
+    }
+}
